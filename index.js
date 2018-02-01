c51460d7b3d95056ba63f17d94bed4c8459f3337
choppari.ravi@gmail.com <choppari.ravi@gmail.com>
2018-02-01 23:14:16 +0530

    my commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 
 
 app.use(express.static(__dirname+"/client/dist/"));
-router.use('/', express.static(__dirname+"/client/dist/", { redirect: false }));
+//router.use('/', express.static(__dirname+"/client/dist/", { redirect: false }));
 
 app.get('*', (req, res)=>{
     // res.send('Hello World!');
